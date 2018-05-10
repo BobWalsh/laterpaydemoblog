c43a0e02541fa339a32e3b0e0d5aff5ea98f575f
Bob Walsh <bob.walsh@47hats.com>
2018-05-10 09:47:01 -0700

    first laterpay commit

---
+++ content/meta/config.js
@@ -0,0 +1,30 @@
+const colors = require("../../src/styles/colors");
+
+module.exports = {
+  siteTitle: "PersonalBlog - a blog starter for GatsbyJS", // <title>
+  shortSiteTitle: "PersonalBlog GatsbyJS Starter", // <title> ending for posts and pages
+  siteDescription: "PersonalBlog is a GatsbyJS starter.",
+  siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
+  pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+  // author
+  authorName: "Bob Walsh",
+  authorTwitterAccount: "bobwalsh",
+  // info
+  infoTitle: "Bob Walsh",
+  infoTitleNote: "personal blog",
+  // manifest.json
+  manifestName: "PersonalBlog - a blog starter for GatsbyJS",
+  manifestShortName: "PersonalBlog", // max 12 characters
+  manifestStartUrl: "/",
+  manifestBackgroundColor: colors.bg,
+  manifestThemeColor: colors.bg,
+  manifestDisplay: "standalone",
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/laterpay" },
+    { name: "twitter", url: "https://twitter.com/laterpay" },
+    { name: "facebook", url: "http://facebook.com/laterpay" }
+  ]
+};
