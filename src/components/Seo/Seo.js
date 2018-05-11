b52b0b035c128fdc3df6f34b69344b2107f78c96
Bob Walsh <bob.walsh@47hats.com>
2018-05-11 10:50:45 -0700

    wip

--- src/components/Seo/Seo.js
+++ src/components/Seo/Seo.js
@@ -25,8 +25,6 @@ const Seo = props => {
       {/* General tags */}
       <title>{title}</title>
       <meta name="description" content={description} />
-      {/* LaterPay Connector */}
-      <script type="text/javascript"   src="https://connector-script.uselaterpay.com/3-stable/us/prod/app-en-us.js"></script>
       {/* OpenGraph tags */}
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
